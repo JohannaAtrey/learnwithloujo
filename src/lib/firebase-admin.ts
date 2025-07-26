6cc57cec99740952d89fdc2a43f546f3f61f9e64
johanna <johannaatrey@gmail.com>
2025-07-26 23:32:57 +0530

    Deployment Bug Fix

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -1,19 +1,12 @@
-import * as admin from 'firebase-admin';
-import { getApps } from 'firebase-admin/app';
+import { initializeApp, applicationDefault, getApps } from 'firebase-admin/app';
 import { getAuth } from 'firebase-admin/auth';
 import { getFirestore } from 'firebase-admin/firestore';
 
 // Check if Firebase Admin has already been initialized
 if (!getApps().length) {
-  const serviceAccount = {
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-    privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
-  };
-
-  // Initialize Firebase Admin with the service account
-  admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
+  initializeApp({
+    credential: applicationDefault(),
+    projectId: "learnwithloujo-testing",
   });
 }
 
